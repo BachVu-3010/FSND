a736caa654e274b465753d3a7fcf3076631a0dc6
Bach Vu <bachvu@1987>
2020-09-25 21:35:13 -0400

    Set up /api/categories api

--- projects/02_trivia_api/starter/backend/app.py
+++ projects/02_trivia_api/starter/backend/app.py
@@ -1,10 +1,11 @@
-import sys
 import os
 
-from models import setup_db, Question, Category
+from .models import setup_db, Question, Category
+from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+
 
 QUESTIONS_PER_PAGE = 10
 
@@ -15,9 +16,27 @@ def create_app():
     setup_db(app)
     
     # Define rest api pulling all categories
-    @app.route("/api/categories", methods = ['GET'])
+    @app.route('/api/categories', methods = ['GET'])
+    def get_all_categories():
 
-    categories = Category.query.all()
-    data = {category.id : category.type for category in categories}
-    return jsonify(data)
+        try: 
 
+            categories = Category.query.all()
+            data = {category.id : category.type for category in categories}
+            return jsonify({
+                "success": True,
+                "categories": data
+            })
+        except:
+            return jsonify({
+                "success": False
+            })
+
+    @app.route("/api")
+    def get_all_questions():
+        pass
+
+    return app
+
+
+# create_app()
