2c66e6a716ec827180975fecc69080d152788e75
Bach Vu <bachvu@1987>
2020-09-27 17:19:44 -0400

    Complete post searchTerm api

--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -23,7 +23,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `api/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `api/questions?page=${this.state.page}`, 
       type: "GET",
       success: (result) => {
         this.setState({
@@ -78,7 +78,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `api/questions`, //TODO: update request URL
+      url: `api/questions`, 
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
