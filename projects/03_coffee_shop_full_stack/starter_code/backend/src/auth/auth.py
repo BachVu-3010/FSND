69ca3e9045998a8a2d174f3f70a1fa078672b134
Bach Vu <bachvu@1987>
2020-10-11 13:19:26 -0400

    Update authenticate file

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 # First, define three constants to communicate with Auth0 to validate users (tokens)
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-0occ77jb.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'coffee_shop'
 
 # AuthError Exception
 # Secondly, define class AuthError to represent errors originated in this module
@@ -38,10 +38,120 @@ class AuthError(Exception):
     return the token part of the header
 '''
 
-
 def get_token_auth_header():
-    raise Exception('Not Implemented')
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
 
+    parts = auth.split()
+    # a valid auth has the form of 'bearer {{string}}'
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+
+
+
+
+
+'''
+@TODO implement verify_decode_jwt(token) method
+    @INPUTS
+        token: a json web token (string)
+
+    it should be an Auth0 token with key_id (kid)
+    it should verify the token using Auth0 /.well-known/jwks.json
+    it should decode the payload from the token
+    it should validate the claims
+    return the decoded payload
+
+    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+'''
+
+
+def verify_decode_jwt(token):
+    # Pull the public key and make sure that the jwt was signed by Auth0
+    # urlopen is a method from url library 
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+
+    # jwk = json web keys. There are two keys.
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    # There are two keys in here
+    for key in jwks['keys']:
+
+        # Choose the key_id that matches with unverified_header_id
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                # kid = k_id = key_id
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            # decode jwt from a correct key
+            payload = jwt.decode(
+                token,
+                # public key to decode the token
+                rsa_key, 
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -57,26 +167,12 @@ def get_token_auth_header():
 
 
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if permission not in payload:
+        raise AuthError({
+            'code': 'invalid_token',
+            'description': 'can not find permission in payload'
+    }, 401)
 
-
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
-
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
-
-
-def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
 
 
 '''
@@ -95,7 +191,7 @@ def verify_decode_jwt(token):
 # Instead of sharing static public keys, Auth0 uses the JWK specification to
 #  represent the cryptographic keys used for signing tokens.
 
-
+# permission_example = 'get:drinks'
 def requires_auth(permission=''):
 
     def requires_auth_decorator(f):
